e980662ce2a70faa9d0cdfd8930343db9f3f0038
Atul Verma <atulverma82@yahoo.com>
2013-04-11 00:37:18 +0530

    fixing file path. adding some info notes for writing migrations

--- examples/db/migrate/201304081758_rename_column.rb
+++ examples/db/migrate/201304081758_rename_column.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 class RenameColumn < MongoidMigrations::Migration
+
   def self.migration_tasks
-    [:rename_name_to_full_name]
+    [:rename_name_to_full_name, :modify_column_value]
   end
 
   def self.rename_name_to_full_name 
@@ -10,13 +11,20 @@ class RenameColumn < MongoidMigrations::Migration
       3.times { |i| test_model_collection.insert({name: "test-#{i}"}) }
       test_model_collection.find("name" => {'$exists' => true}).update_all({"$rename" => {"name" => "full_name"}})
     end
+  end
 
+  def self.modify_column_value
     session_for :superman do |collections|
       hulk_collection = collections[:hulks]
 
       3.times { |i| hulk_collection.insert({state: "enlarged-#{i}"}) }
 
-      hulk_collection.find({'state' => /enlarged.*/i}).select({_id: 1, state: 1}).each do |doc|
+      #1.select only columns you need
+      #2.no_timeout so long running queries dont timeout
+      #3.dont replace old value with new value unless you can recreate old values.
+      #4.better to rename old column (_old) and add a new column with new value
+
+      hulk_collection.find({'state' => /enlarged.*/i}).no_timeout.select({_id: 1, state: 1}).each do |doc|
         doc["state"] = doc["state"].sub("enlarged", "expanded")
         hulk_collection.find({"_id" => doc["_id"]}).update(doc)
       end
@@ -28,13 +36,16 @@ class RenameColumn < MongoidMigrations::Migration
       test_model_collection = collections[:test_models]
       test_model_collection.find("full_name" => {'$exists' => true}).update_all({"$rename" => {"full_name" => "name"}})
     end
+  end
 
+  def self.rollback_modify_column_value
     session_for :superman do |collections|
       hulk_collection = collections[:hulks]
-       hulk_collection.find({'state' => /expanded.*/i}).each do |doc| 
+      hulk_collection.find({'state' => /expanded.*/i}).each do |doc| 
         doc["state"] = doc["state"].sub("expanded", "enlarged")
         hulk_collection.find({"_id" => doc["_id"]}).update(doc)
-       end
+      end
     end
   end
+
 end
